46f3aaf442b2a00166f5a97e7f2c275e403dbfc3
mfranczy <marcin0franczyk@gmail.com>
2017-10-29 21:15:37 +0100

    make module works for kernel 4.13.8

--- kfilter.c
+++ kfilter.c
@@ -4,17 +4,21 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/netfilter_ipv6.h>
 
+#include <net/net_namespace.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Marcin Franczyk");
-MODULE_DESCRIPTION("Packets fitler");
+MODULE_DESCRIPTION("Packets filter");
 
 
 static struct nf_hook_ops nfho; 
 
+
 unsigned int net_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
     // accept everything
     // in progress..
+    printk(KERN_INFO "Test: protocol %d", skb->protocol);
     return NF_ACCEPT;
 };
 
@@ -22,19 +26,19 @@ static int filter_init(void) {
     printk(KERN_INFO "Initializing kfilter module");
 
     nfho.hook = net_hook;
-    nfho.hooknum = NF_IP_PRE_ROUTING;
+    nfho.hooknum = NF_INET_PRE_ROUTING;
     nfho.pf = PF_INET;
     nfho.priority = NF_IP_PRI_FIRST;
 
-    nf_register_hook(&nfho);
+    nf_register_net_hook(&init_net, &nfho);
 
     printk(KERN_INFO "kfilter has started");
     return 0;
 }
 
 static void filter_exit(void) {
-    nf_unregister_hook(&nfho);
-    printk(KERN_INFO "filter modul has been removed!\n");
+    nf_unregister_net_hook(&init_net, &nfho);
+    printk(KERN_INFO "kfilter has been removed");
 }
 
 module_init(filter_init);
